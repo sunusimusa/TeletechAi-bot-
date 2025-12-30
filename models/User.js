7ad26a74f013c36b5dc43432f2899fa1b1014a73
sunusimusa <78286160+sunusimusa@users.noreply.github.com>
2025-12-30 19:05:20 +0100

    User.js

--- models/User.js
+++ models/User.js
@@ -1,1 +1,34 @@
+const mongoose = require("mongoose");
 
+const userSchema = new mongoose.Schema({
+  telegramId: { type: String, required: true },
+
+  balance: { type: Number, default: 0 },
+  token: { type: Number, default: 0 },
+  level: { type: Number, default: 1 },
+
+  energy: { type: Number, default: 100 },
+  lastEnergyUpdate: { type: Number, default: Date.now },
+
+  lastDaily: { type: Number, default: 0 },
+  lastBox: { type: Number, default: 0 },
+
+  spinCount: { type: Number, default: 1 },
+  lastSpin: { type: Number, default: 0 },
+
+  refBy: { type: String, default: null },
+  referrals: { type: Number, default: 0 },
+
+  adsSpinCount: { type: Number, default: 0 },
+  lastAdsSpin: { type: Number, default: 0 },
+
+  tasks: {
+    youtube: { type: Boolean, default: false },
+    channel: { type: Boolean, default: false },
+    group: { type: Boolean, default: false }
+  },
+
+  teamId: { type: String, default: null }
+}, { timestamps: true });
+
+module.exports = mongoose.model("User", userSchema);
