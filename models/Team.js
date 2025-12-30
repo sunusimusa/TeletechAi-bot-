8f8d581e98dbd432fd483a2e8198ecbe698aa853
sunusimusa <78286160+sunusimusa@users.noreply.github.com>
2025-12-30 19:08:28 +0100

    Team.js

--- models/Team.js
+++ models/Team.js
@@ -1,1 +1,11 @@
+const mongoose = require("mongoose");
 
+const teamSchema = new mongoose.Schema({
+  name: String,
+  leader: String,
+  members: [String],
+  totalScore: { type: Number, default: 0 },
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model("Team", teamSchema);
