7b75c541cfb12e8ceef0a1f80476c37716739139
sunusimusa <78286160+sunusimusa@users.noreply.github.com>
2026-01-01 17:28:33 +0100

    User.js

--- models/User.js
+++ models/User.js
@@ -1,1 +1,12 @@
+import mongoose from "mongoose";
 
+const userSchema = new mongoose.Schema({
+  telegramId: { type: String, unique: true },
+  balance: { type: Number, default: 0 },
+  energy: { type: Number, default: 100 },
+  freeTries: { type: Number, default: 3 },
+  tokens: { type: Number, default: 0 },
+  lastEnergy: { type: Date, default: Date.now }
+});
+
+export default mongoose.model("User", userSchema);
