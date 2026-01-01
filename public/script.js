68019fe0f61ce24ac52f0dcb5b5e96702ccb2503
sunusimusa <78286160+sunusimusa@users.noreply.github.com>
2026-01-01 11:24:08 +0100

    script.js

--- public/script.js
+++ public/script.js
@@ -1,1 +1,19 @@
+async function openBox(id) {
+  const res = await fetch("/api/open-box", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      userId: "demo_user"
+    })
+  });
 
+  const data = await res.json();
+
+  if (data.error) {
+    alert(data.error);
+    return;
+  }
+
+  document.getElementById("result").innerText =
+    "🎉 You got: " + data.reward;
+}
