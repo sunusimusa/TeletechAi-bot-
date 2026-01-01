4d8340cae6e105655845e6576551b45435f3f798
sunusimusa <78286160+sunusimusa@users.noreply.github.com>
2026-01-01 21:37:44 +0100

    User.js

--- models/User.js
+++ models/User.js
@@ -4,26 +4,23 @@ const UserSchema = new mongoose.Schema({
   telegramId: { type: String, unique: true },
 
   balance: { type: Number, default: 0 },
-  tokens: { type: Number, default: 0 },
-
-  referralCode: { type: String, unique: true },
-  referredBy: { type: String, default: null },
-  referralsCount: { type: Number, default: 0 }
-
   energy: { type: Number, default: 100 },
   freeTries: { type: Number, default: 3 },
+  tokens: { type: Number, default: 0 },
+
+  referralCode: String,
+  referredBy: String,
+  referralsCount: { type: Number, default: 0 },
 
   lastEnergy: { type: Number, default: Date.now },
   lastDaily: { type: Number, default: 0 },
 
-  withdrawals: [
-    {
-      amount: Number,
-      wallet: String,
-      status: { type: String, default: "pending" },
-      date: { type: Date, default: Date.now }
-    }
-  ]
+  withdrawals: [{
+    amount: Number,
+    wallet: String,
+    status: { type: String, default: "pending" },
+    date: { type: Date, default: Date.now }
+  }]
 });
 
 export default mongoose.model("User", UserSchema);
