c785ad90c41cc4e759d55c47911de37ac74f3a41
sunusimusa <78286160+sunusimusa@users.noreply.github.com>
2026-01-03 16:25:01 +0100

    ton.js

--- blockchain/ton.js
+++ blockchain/ton.js
@@ -1,1 +1,17 @@
+// blockchain/ton.js
+import TonWeb from "tonweb";
+import nacl from "tweetnacl";
 
+const tonweb = new TonWeb(
+  new TonWeb.HttpProvider(process.env.RPC_URL)
+);
+
+const seed = Buffer.from(process.env.PRIVATE_KEY, "hex");
+const keyPair = nacl.sign.keyPair.fromSeed(seed);
+
+const wallet = tonweb.wallet.create({
+  publicKey: keyPair.publicKey,
+  wc: 0
+});
+
+export { tonweb, wallet, keyPair };
